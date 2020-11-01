178fb1317455718c613064924aa7c7cc18196da9
LuisaFRojas <66465943+LuisaFRojas@users.noreply.github.com>
2020-11-01 15:23:14 -0600

    Added sout to class.

--- src/test/java/TC_Luisa.java
+++ src/test/java/TC_Luisa.java
@@ -1,5 +1,6 @@
 public class TC_Luisa {
     public static void main(String[] args) {
         System.out.println("Working on my test case");
+        System.out.println("Hey guys, this is for my second pull request!");
     }
 }
