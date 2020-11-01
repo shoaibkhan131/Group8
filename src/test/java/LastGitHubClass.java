ae86befe573a634f27215b36118a2ceb245626fd
anam88 <amasroor88@gmail.com>
2020-11-01 15:27:45 -0600

    added two print statements in LastGitHubClass.java

--- src/test/java/LastGitHubClass.java
+++ src/test/java/LastGitHubClass.java
@@ -1,3 +1,7 @@
 public class LastGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("I have learned interesting stuff in Git & GitHub class");
+        System.out.println("Still confuse about many things in Git & GitHub class");
+    }
 
 }
